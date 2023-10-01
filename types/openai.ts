7091ee23bb0bf380fd81ace13b7d355ec46e2551
Raiko Funakami <raiko.funakami@access-company.com>
2023-10-01 22:38:23 +0900

    add model

--- types/openai.ts
+++ types/openai.ts
@@ -12,10 +12,12 @@ export enum OpenAIModelID {
   GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_4 = 'gpt-4',
   GPT_4_32K = 'gpt-4-32k',
+  CUSTOM_1 = 'acsmine-search',
+  CUSTOM_2 = 'summary-conversation-chat',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.CUSTOM_1;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_3_5]: {
@@ -41,5 +43,17 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     name: 'GPT-4-32K',
     maxLength: 96000,
     tokenLimit: 32000,
+  },
+  [OpenAIModelID.CUSTOM_1]: {
+    id: OpenAIModelID.CUSTOM_1,
+    name: 'SIMPLE',
+    maxLength: 12000,
+    tokenLimit: 3000,
+  },
+  [OpenAIModelID.CUSTOM_2]: {
+    id: OpenAIModelID.CUSTOM_2,
+    name: 'SUMMARY',
+    maxLength: 12000,
+    tokenLimit: 3000,
   },
 };
